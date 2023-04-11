979d673bc3ae8449ae3e04bcd4d14439d7c3ca05
Vinícius <matosvb@gmail.com>
2023-04-11 19:31:11 -0300

    Update documentation

---
+++ README.md
@@ -0,0 +1,18 @@
+# BLAB Chatbot - OpenAI
+
+This Python module allows the integration of [OpenAI](../../../deepage) models with
+[BLAB](../../../blab-controller).
+
+
+### Installation
+
+- Obtain an [OpenAI API key](https://openai.com/blog/openai-api).
+
+- Install a version of
+  [Python](https://www.python.org/downloads/release/python-3100/) ≥ 3.10.
+
+- Follow [these installation instructions](../../../blab-chatbot-bot-client/blob/main/INSTALL.md)
+  using [*settings_example_template.py*](settings_openai_template.py) as a template.
+
+- Follow [these instructions](../../../blab-chatbot-bot-client/blob/main/RUN.md) to execute the
+  program.
--- settings_openai_TEMPLATE.py
+++ settings_openai_TEMPLATE.py
@@ -1,31 +1,45 @@
-"""This module contains settings for Haystack."""
+"""This module contains settings for OpenAI."""
 
 from __future__ import annotations
 
 from typing import Any
 
+# fmt: off
+
+
+BLAB_CONNECTION_SETTINGS: dict[str, str | int] = {
+
+    # Address of the local HTTP server that the controller will connect to:
+    "BOT_HTTP_SERVER_HOSTNAME": "127.0.0.1",
+
+    # Port of the aforementioned server:
+    "BOT_HTTP_SERVER_PORT": 25223,
+
+    # BLAB Controller address for WebSocket connections:
+    "BLAB_CONTROLLER_WS_URL": "ws://localhost:8000",
+
+}
+
+
 OPENAI_SETTINGS: dict[str, Any] = {
-    # OpenAI API key
+
+    # OpenAI API key (possibly dynamically obtained from a safe storage location):
     "API_KEY": "...........",
-    #
-    # "openai.ChatCompletion" or "openai.Completion", depending on the model
-    "COMPLETION_CLASS": "",
-    #
-    # number of previous messages to send to the server (only for ChatCompletion):
+
+    # "openai.ChatCompletion" or "openai.Completion", depending on the model:
+    "COMPLETION_CLASS": "openai.ChatCompletion",
+
+    # Number of previous messages to send to the server (only for ChatCompletion):
     "HISTORY_SIZE": 10,
-    #
-    # arguments to create the completion instance - see documentation:
-    # https://platform.openai.com/docs/api-reference/completions/create?lang=python
+
+    # Arguments to create the completion instance (see documentation at:
+    # <https://platform.openai.com/docs/api-reference/completions/create?lang=python>
     # and
-    # https://platform.openai.com/docs/api-reference/chat/create?lang=python
+    # <https://platform.openai.com/docs/api-reference/chat/create?lang=python>):
     "COMPLETION_CREATE_ARGS": {
         "model": "text-davinci-003",
         "max_tokens": 25,
     },
+
 }
 
-BLAB_CONNECTION_SETTINGS: dict[str, str | int] = {
-    "BOT_HTTP_SERVER_HOSTNAME": "localhost",
-    "BOT_HTTP_SERVER_PORT": 25223,
-    "BLAB_CONTROLLER_WS_URL": "ws://localhost:8000",
-}
